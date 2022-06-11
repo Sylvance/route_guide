7d8521a5a8ec52b2af4c710d94282fe7da6209bf
sylvance_theone <9350722+Sylvance@users.noreply.github.com>
2022-06-11 15:30:39 +0300

    [route_guide] Initial setup

---
+++ lib/route_guide.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "route_guide/version"
+
+module RouteGuide
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/route_guide_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RouteGuide do
+  it "has a version number" do
+    expect(RouteGuide::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
