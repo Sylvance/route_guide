7e856286f2436658331430f2df674cb66bd0bafd
sylvance_theone <9350722+Sylvance@users.noreply.github.com>
2022-06-11 18:18:58 +0300

    [route_guide] Remove unused test

--- spec/route_guide_spec.rb
+++ spec/route_guide_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RouteGuide do
   it "has a version number" do
     expect(RouteGuide::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
