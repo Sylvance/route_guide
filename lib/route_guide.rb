d34d350f50393eae6cf1ab7721d7379f8cbff0e3
sylvance_theone <9350722+Sylvance@users.noreply.github.com>
2022-06-11 19:38:22 +0300

    [route_guide] Bump version

--- lib/route_guide.rb
+++ lib/route_guide.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "route_guide/version"
+require_relative "route_guide/route_guide_pb"
+require_relative "route_guide/route_guide_services_pb"
 
 module RouteGuide
   class Error < StandardError; end
